1241dd69a8995576bc385a2bea9a27e5c1bab874
Ryosuke Uchiyama <uchi63rs@yahoo.co.jp>
2020-09-06 18:23:02 +0900

    spped up

--- Sudoku.py
+++ Sudoku.py
@@ -1,18 +1,31 @@
 import numpy as np
 
+# 盤面を二次元ベクトルで表す
+ALL_NUMBERS = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 class Sudoku:
     """
     Sudoku model class
     """
-    # Numpy field (-1 as undefined cell value)
-    field = np.full((9, 9), -1)
+    # Sudoku field
+    field = np.array([[]])
+
+    # nums[x][y][v] ← number of v+1 value in row, column and block containing (x, y)
+    nums = np.array([[[]]])
+
+    # choices[x][y] ← choices can be fill (x, y)
+    choices = np.array([[]])
 
     def __init__(self):
         """
         Constructor
         """
-        pass
+        self.field = np.full((9, 9), -1)
+        self.nums = np.full((9, 9, 9), 0)
+        self.choices = np.full((9, 9), set())
+        for x in range(0, 9):
+            for y in range(0, 9):
+                self.choices[x][y] = ALL_NUMBERS.copy()
     
     def get(self):
         """
@@ -27,18 +40,27 @@ class Sudoku:
     
     def find_empty(self):
         """
-        Get first empty cell from left top
+        Get cell with the fewest choices in empty cells
 
         Returns
         -------
         (int, int)
             Index of empty cell (if not found return (-1, -1))
         """
+        min_num_choices = 10
+        ret_x, ret_y = (-1, -1)
+
         for x in range(0, 9):
             for y in range(0, 9):
-                if self.field[x][y] == -1:
-                    return (x, y)
-        return (-1, -1)
+                if self.field[x][y] != -1:
+                    continue
+
+                if (min_num_choices > len(self.choices[x][y])):
+                    min_num_choices = len(self.choices[x][y])
+                    ret_x = x
+                    ret_y = y
+
+        return (ret_x, ret_y)
 
     def find_choices(self, x, y):
         """
@@ -53,32 +75,62 @@ class Sudoku:
 
         Returns
         -------
-        list<int>
+        set<int>
             Available choices at (x, y)
         """
-        can_use = {i + 1 for i in range(0, 9)}
-
-        # Horizontal check
-        for i in range(0, 9):
-            if self.field[x][i] != -1:
-                can_use.discard(self.field[x][i])
-        
-        # Vertical check
-        for i in range(0, 9):
-            if self.field[i][y] != -1:
-                can_use.discard(self.field[i][y])
-        
-        # Box check
-        cx = int(int(x / 3) * 3) + 1
-        cy = int(int(y / 3) * 3) + 1
-        for i in range(cx - 1, cx + 2):
-            for j in range(cy - 1, cy + 2):
-                if self.field[i][j] != -1:
-                    can_use.discard(self.field[i][j])
-        
-        return can_use
+        return self.choices[x][y].copy()
     
-    def put(self, x, y, value):
+    def print(self):
+        """
+        Print formatted sudoku board
+        """
+        print("    0 1 2   3 4 5   6 7 8  ")
+        print("  -------------------------")
+        for x in range(0, 9):
+            print(f"{x} | ", end="")
+            for y in range(0, 9):
+                if self.field[x][y] == -1:
+                    print("* ", end="")
+                else:
+                    print(f"{self.field[x][y]} ", end="")
+                if y % 3 == 2:
+                    print("| ", end="")
+            print("")
+            if x % 3 == 2:
+                print("  -------------------------")
+
+    def put_detail(self, x, y, val, x2, y2):
+        """
+        Impact to (x2, y2) on putting val in (x, y)
+
+        Parameters
+        ----------
+        x : int
+            Source row index
+        y : int
+            Source column index
+        value : int
+            Value
+        x2 : int
+            Target row index
+        y2 : int
+            Target column index
+        """
+        if x == x2 and y == y2:
+            return
+        
+        # Nothing to do if (x2, y2) has already value
+        if self.field[x2][y2] != -1:
+            return
+        
+        # Remove from choice if there are no val at impact range of (x2, y2)
+        if self.nums[x2][y2][val - 1] == 0:
+            self.choices[x2][y2].discard(val)
+        
+        # Update nums
+        self.nums[x2][y2][val - 1] += 1
+
+    def put(self, x, y, val):
         """
         Put value
 
@@ -91,7 +143,52 @@ class Sudoku:
         value : int
             Detected cell value
         """
-        self.field[x][y] = value
+        # Put value
+        self.field[x][y] = val
+
+        # Update impact to row, column and block containing (x, y)
+        for i in range(0, 9):
+            self.put_detail(x, y, val, x, i)
+        
+        for i in range(0, 9):
+            self.put_detail(x, y, val, i, y)
+        
+        cx = int(int(x / 3) * 3) + 1
+        cy = int(int(y / 3) * 3) + 1
+        for i in range(cx - 1, cx + 2):
+            for j in range(cy - 1, cy + 2):
+                self.put_detail(x, y, val, i, j)
+    
+    def reset_detail(self, x, y, val, x2, y2):
+        """
+        Impact to (x2, y2) on removing val from (x, y)
+
+        Parameters
+        ----------
+        x : int
+            Source row index
+        y : int
+            Source column index
+        value : int
+            Value
+        x2 : int
+            Target row index
+        y2 : int
+            Target column index
+        """
+        if x == x2 and y == y2:
+            return
+
+        # Nothing to do if (x2, y2) has already value
+        if self.field[x2][y2] != -1:
+            return
+        
+        # Update nums
+        self.nums[x2][y2][val - 1] -= 1
+
+        # Regenerate val to choice if nums gets 0
+        if self.nums[x2][y2][val - 1] == 0:
+            self.choices[x2][y2].add(val)
 
     def reset(self, x, y):
         """
@@ -104,4 +201,74 @@ class Sudoku:
         y : int
             Column index
         """
+        # Update impact to row, column and block containing (x, y)
+        val = self.field[x][y]
+
+        for i in range(0, 9):
+            self.reset_detail(x, y, val, x, i)
+        
+        for i in range(0, 9):
+            self.reset_detail(x, y, val, i, y)
+
+        cx = int(int(x / 3) * 3) + 1
+        cy = int(int(y / 3) * 3) + 1
+        for i in range(cx - 1, cx + 2):
+            for j in range(cy - 1, cy + 2):
+                self.reset_detail(x, y, val, i, j)
+        
+        # Remove value
         self.field[x][y] = -1
+
+    def process(self):
+        """
+        Fill cells uniquely determined
+        """
+        # Process value 1, 2, ..., 9 in order
+        for val in range(1, 10):
+            # For each row
+            for x in range(0, 9):
+                exist = False
+                can_enter = []
+                for y in range(0, 9):
+                    if self.field[x][y] == val:
+                        exist = True
+                    if self.field[x][y] == -1 and val in self.choices[x][y]:
+                        can_enter.append(y)
+
+                # Put val if only one cell can do
+                if not exist and len(can_enter) == 1:
+                    y = can_enter[0]
+                    self.put(x, y, val)
+                
+            # For each column
+            for y in range(0, 9):
+                exist = False
+                can_enter = []
+                for x in range(0, 9):
+                    if self.field[x][y] == val:
+                        exist = True
+                    if self.field[x][y] == -1 and val in self.choices[x][y]:
+                        can_enter.append(x)
+
+                # Put val in only one cell can do
+                if not exist and len(can_enter) == 1:
+                    x = can_enter[0]
+                    self.put(x, y, val)
+            
+            # For each block
+            for bx in range(0, 3):
+                for by in range(0, 3):
+                    exist = False
+                    can_enter = []
+                    for x in range(bx * 3, (bx + 1) * 3):
+                        for y in range(by * 3, (by + 1) * 3):
+                            if self.field[x][y] == val:
+                                exist = True
+                            if self.field[x][y] == -1 and val in self.choices[x][y]:
+                                can_enter.append((x, y))
+                    
+                    # Put val if only one cell can do
+                    if not exist and len(can_enter) == 1:
+                        x = can_enter[0][0]
+                        y = can_enter[0][1]
+                        self.put(x, y, val)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import copy
 import numpy as np
 
 
@@ -25,16 +26,49 @@ def dfs(board, res):
     list<Sudoku>
         All solved Sudoku boards
     """
+    # Keep Sudoku board status
+    board_prev = copy.deepcopy(board)
+
+    # Fill cells uniquely determined
+    board.process()
+
     x, y = board.find_empty()
 
     if x == y == -1:
         res.append(board.get())
+        # Undo before return
+        board = copy.deepcopy(board_prev)
         return
-    
+
     for value in board.find_choices(x, y):
         board.put(x, y, value)
         dfs(board, res)
         board.reset(x, y)
+        # For Python recursive call method
+        board = copy.deepcopy(board_prev)
+    
+    # Undo
+    board = copy.deepcopy(board_prev)
+
+
+def format_print(board):
+    """
+    Print formatted sudoku board
+    """
+    print("    0 1 2   3 4 5   6 7 8  ")
+    print("  -------------------------")
+    for x in range(0, 9):
+        print(f"{x} | ", end="")
+        for y in range(0, 9):
+            if board[x][y] == -1:
+                print("* ", end="")
+            else:
+                print(f"{board[x][y]} ", end="")
+            if y % 3 == 2:
+                print("| ", end="")
+        print("")
+        if x % 3 == 2:
+            print("  -------------------------")
 
 
 def format_board(board):
@@ -75,36 +109,58 @@ def main(args):
         Command line arguments
     """
     src = args.src
+    pattern = args.pattern
+    src_path = Path(src)
 
-    with open(src, "r") as f:
-        src_data = [s.strip() for s in f.readlines()]
+    if not src_path.exists():
+        raise FileNotFoundError("Path \"src\" is not a file or directory")
 
-    board = Sudoku()
-    for x in range(0, 9):
-        for y in range(0, 9):
-            if src_data[x][y] == "*":
-                continue
-            board.put(x, y, int(src_data[x][y]))
+    file_list = []
+    if src_path.is_dir():
+        file_list = list(src_path.glob(pattern))
+
+    elif src_path.is_file():
+        file_list = [src_path]
+
+    elapsed_time = 0.0
+    for one_src_path in file_list:
+        with open(one_src_path, "r") as f:
+            src_data = [s.strip() for s in f.readlines()]
+
+        board = Sudoku()
+        for x in range(0, 9):
+            for y in range(0, 9):
+                if src_data[x][y] == "*":
+                    continue
+                board.put(x, y, int(src_data[x][y]))
     
-    results = []
-    start_time = time.time()
-    dfs(board, results)
-    end_time = time.time()
-    print(f"{len(results)} results, {end_time - start_time:.4f} sec")
+        results = []
+        start_time = time.time()
+        dfs(board, results)
+        end_time = time.time()
+        elapsed_time += end_time - start_time
+        print(f"File: {one_src_path.name}")
+        print(f"{len(results)} results, {end_time - start_time:.4f} sec")
 
-    if not results:
-        print("No solutions.")
+        if not results:
+            print("No solutions.")
 
-    else:
-        i = 1
-        for result in results:
-            print(f"No.{i}")
-            print(format_board(result))
-            i += 1
+        else:
+            i = 1
+            for result in results:
+                print(f"No.{i}")
+                print(format_board(result))
+                i += 1
+    
+    print("--------------------------------------------------")
+    print("Complete!!")
+    print("--------------------------------------------------")
+    print(f"Elapsed Time: {elapsed_time:.4f} sec")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Solve Sudoku (number place) with DFS (backtracking)")
     parser.add_argument("src", help="Source file path")
+    parser.add_argument("-p", "--pattern", default="*.txt", help="Source filename pattern (by default \"*.txt\")")
     args = parser.parse_args()
     main(args)
